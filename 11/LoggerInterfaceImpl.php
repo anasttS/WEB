cd57558e0c2a620406b33fcef5504e0013b7723a
Anastasia <tokod684@mail.ru>
2020-05-21 15:17:42 +0500

    fix 11 hw

--- 11/LoggerInterfaceImpl.php
+++ 11/LoggerInterfaceImpl.php
@@ -7,7 +7,7 @@ use loggerInterface\LoggerInterface;
 
 require "LoggerInterface.php";
 
-class LoggerInterfaceImpl implements LoggerInterface
+class LoggerInterfaceImpl  implements LoggerInterface
 {
     private array $json_arr = [];
     private $file;
@@ -19,9 +19,105 @@ class LoggerInterfaceImpl implements LoggerInterface
 
     public function log($level, $message, array $context = [])
     {
+        switch ($level) {
+            case "emergency":
+                $this->emergency($message, $context);
+                break;
+            case "alert":
+                $this->alert($message, $context);
+                break;
+            case "critical":
+                $this->critical($message, $context);
+                break;
+            case "error":
+                $this->error($message, $context);
+                break;
+            case "warning":
+                $this->warning($message, $context);
+                break;
+            case "notice":
+                $this->notice($message, $context);
+                break;
+            case "info":
+                $this->info($message, $context);
+                break;
+            case "debug":
+                $this->debug($message);
+                break;
+            default:
+                print "Unknown level";
+
+        }
+    }
+
+    public function emergency($message, array $context = [])
+    {
         $now = new DateTime('now');
         array_push($this->json_arr, [
-            'type' => $level,
+            'type' => "emergency",
+            'time' => $now,
+            'context' => $context]);
+    }
+
+    public function alert($message, array $context = [])
+    {
+        $now = new DateTime('now');
+        array_push($this->json_arr, [
+            'type' => "alert",
+            'time' => $now,
+            'context' => $context]);
+    }
+
+    public function critical($message, array $context = [])
+    {
+        $now = new DateTime('now');
+        array_push($this->json_arr, [
+            'type' => "critical",
+            'time' => $now,
+            'context' => $context]);
+    }
+
+    public function error($message, array $context = [])
+    {
+        $now = new DateTime('now');
+        array_push($this->json_arr, [
+            'type' => "error",
+            'time' => $now,
+            'context' => $context]);
+    }
+
+    public function warning($message, array $context = [])
+    {
+        $now = new DateTime('now');
+        array_push($this->json_arr, [
+            'type' => "warning",
+            'time' => $now,
+            'context' => $context]);
+    }
+
+    public function notice($message, array $context = [])
+    {
+        $now = new DateTime('now');
+        array_push($this->json_arr, [
+            'type' => "notice",
+            'time' => $now,
+            'context' => $context]);
+    }
+
+    public function info($message, array $context = [])
+    {
+        $now = new DateTime('now');
+        array_push($this->json_arr, [
+            'type' => "info",
+            'time' => $now,
+            'context' => $context]);
+    }
+
+    public function debug($message, array $context = [])
+    {
+        $now = new DateTime('now');
+        array_push($this->json_arr, [
+            'type' => "debug",
             'time' => $now,
             'context' => $context]);
     }
@@ -29,37 +125,5 @@ class LoggerInterfaceImpl implements LoggerInterface
     public function __destruct()
     {
         fwrite($this->file, json_encode($this->json_arr, JSON_PRETTY_PRINT));
-    }
-
-    public function emergency($message, array $context = [])
-    {
-    }
-
-    public function alert($message, array $context = [])
-    {
-    }
-
-    public function critical($message, array $context = [])
-    {
-    }
-
-    public function error($message, array $context = [])
-    {
-    }
-
-    public function warning($message, array $context = [])
-    {
-    }
-
-    public function notice($message, array $context = [])
-    {
-    }
-
-    public function info($message, array $context = [])
-    {
-    }
-
-    public function debug($message, array $context = [])
-    {
     }
 }
--- 11/index.php
+++ 11/index.php
@@ -1,9 +1,8 @@
 <?php
 
 use loggerImpl\LoggerInterfaceImpl;
-
 require "LoggerInterfaceImpl.php";
 
 $LoggerInterfaceImpl = new LoggerInterfaceImpl("log");
-    $context = array("username" => "Nastya");
-    $LoggerInterfaceImpl->log("log", "log", $context);
+$context = array("username" => "Nastya");
+$LoggerInterfaceImpl->log("info", "User signed up", $context);
